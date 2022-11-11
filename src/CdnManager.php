9759ceee4446f0ce57d48c55a6f6ffff371a198b
Genercode <72575486+autocode2020@users.noreply.github.com>
2022-11-11 19:58:14 +0000

    Create CdnManager.php

---
+++ src/CdnManager.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Services\FileManager;
+
+use GenerCodeOrm\GenerCodeContainer as Container;
+
+#[AsCommand(name: 'gc:cdn')]
+class CdnManager extends GenericCommand
+{
+    protected static $defaultDescription = 'Pushes public files to cdn';
+    protected static $defaultName = "gc:cdn";
+   
+
+    public function configure(): void
+    {
+        parent::configure();
+        $this
+            // the command help shown when running the command with the "--help" option
+            ->setHelp('Push public files to cdn and create invalidations')
+        ;
+   }
+
+
+
+    public function uploadFiles($dir_path) {
+        $fileHandler = $this->app->make(\GenerCodeOrm\FileHandler::class);
+        $dir = new \DirectoryIterator($dir_path);
+
+        $invalidations = [];
+        foreach ($dir as $fileinfo) {
+            if (!$fileinfo->isDot() AND !$fileinfo->isDir()) {
+
+                $real_path = $fileinfo->getRealPath();
+                $relative_path = substr($real_path, strlen($this->repo));
+
+                $new_path = ltrim($relative_path, "/");
+
+                $fileHandler->put($new_path, file_get_contents($real_path));
+                $invalidations[] = "/" . $new_path;
+            }
+        }
+        return $invalidations;
+    }
+
+    public function runInvalidations($invalidations) {
+        $cfClient = new \Aws\CloudFront\CloudFrontClient([
+            'version' => 'latest',
+            'region' => 'eu-west-1'
+        ]);
+
+        $cfClient->createInvalidation([
+            'DistributionId' =>$this->app->config->get("hosting.cfdistid"),
+            "InvalidationBatch" => [
+                "CallerReference" => time(),
+                "Paths" => [
+                    "Items" => $invalidations,
+                    "Quantity" => count($invalidations)
+                ]
+            ]
+        ]);
+    }
+
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        return $this->executeWrapper($input, $output, function ($input, $output) {
+            
+            $invalidations = $this->uploadFiles($this->repo);
+            $invalidations = array_merge($invalidations, $this->uploadFiles($this->repo . "/dist"));
+            $this->runInvalidations($invalidations);
+        });    
+    }
+
+}
